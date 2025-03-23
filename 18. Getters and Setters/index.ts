14b9688748cfc2114e2bc69d9a820eb632c9e2b1
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2025-03-23 19:31:18 +0530

    Getters and Setters

---
+++ 18. Getters and Setters/index.ts
@@ -0,0 +1,28 @@
+class User{
+
+    private _courseCount = 1
+
+    constructor(
+        public email: string,
+        public name: string
+    ){}
+
+    private deleteToken(){
+        console.log("Token deleted")
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNum){
+        if(courseNum<=1){
+            throw new Error("Course count must be greater than one")
+        }
+        this._courseCount = courseNum
+    }
+}
